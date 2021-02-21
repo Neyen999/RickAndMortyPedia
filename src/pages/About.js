a8284b36db1d1a347752da8fb5c61f6bebe96d6d
Neyen Marinelli <neyen_gt@hotmail.com>
2021-02-21 18:14:59 -0300

    Adding templates and pages

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+const About = () => {
+  const view = `
+    <div class="About">
+        <div class="About--info">
+            <h2>Información Detallada Sobre Esta Página</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi fugit numquam blanditiis consectetur aliquam accusamus, beatae eveniet deserunt possimus inventore nemo magnam accusantium nostrum veniam dolorem reiciendis natus tempore, vel corrupti magni a sint! Incidunt, aspernatur in rem non libero autem explicabo mollitia quibusdam eius, possimus soluta deserunt cupiditate dignissimos!</p>
+        </div>
+        <div class="About--contact">
+            <ul>
+                <li><a href="#"><img src="social" alt="" class="About--media"></a></li>
+                <li><a href="#"><img src="social" alt="" class="About--media"></a></li>
+                <li><a href="#"><img src="social" alt="" class="About--media"></a></li>
+            </ul>
+        </div>
+    </div>
+  `;
+  return view;
+};
+
+export default About;
