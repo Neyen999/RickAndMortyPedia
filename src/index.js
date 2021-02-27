263c70f13c0ab019c9ca489831cb4a264ab97762
Neyen Marinelli <neyen_gt@hotmail.com>
2021-02-26 23:12:52 -0300

    I dont know

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Here i import router, and then created an element that will listen when the page is loaded, and will push the route that we need
 import css from "./styles/style.css";
-import router from "./routes";
+import router from "./routes/index";
 // import About from "./pages/About";
 
 window.addEventListener("load", router);
